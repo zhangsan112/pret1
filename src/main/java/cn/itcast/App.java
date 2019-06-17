35366a8af79d0c9888da9a59f8519bb363b56f8a
ssm <12412@itcast.cn>
2019-06-17 16:34:02 +0800

    第一次文件

---
+++ src/main/java/cn/itcast/App.java
@@ -0,0 +1,18 @@
+package cn.itcast;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+
+    public class Aut {
+        System.out.println("Hello World!");
+    }
+}
